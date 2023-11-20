5eef38bef73ff978d6cbd0c06fce704c9f1b5537
vitalii-minchuk <96005884+vitalii-minchuk@users.noreply.github.com>
2023-11-20 09:13:49 +0200

    Dev (#3)
    * add auth and jwt
    
    * add contracts lib

--- apps/account/src/app/configs/mongo.config.ts
+++ apps/account/src/app/configs/mongo.config.ts
@@ -11,18 +11,8 @@ export const getMongoConfig = (): MongooseModuleAsyncOptions => {
   }
 }
 
-const getMongoString = (configService: ConfigService) => `mongodb+srv://mcmintsch:${configService.get('MONGO_PASSWORD')}@cluster0.zryzdr2.mongodb.net/`
-  // 'mongodb://' +
-  // configService.get('MONGO_LOGIN') +
-  // ':' +
-  // configService.get('MONGO_PASSWORD') +
-  // '@' +
-  // configService.get('MONGO_HOST') +
-  // ':' +
-  // configService.get('MONGO_PORT') +
-  // '/' +
-  // configService.get('MONGO_DATABASE') +
-  // '?authSource=' +
-  // configService.get('MONGO_AUTHDATABASE')
-
-
+const getMongoString = (configService: ConfigService) => {
+  return 'mongodb+srv://mcmintsch:' +
+    configService.get('MONGO_PASSWORD') +
+    '@cluster0.zryzdr2.mongodb.net/'
+}
